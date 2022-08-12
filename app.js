25c4af24df40291d6a59d8a3217ea09cefdbed4b
JustKel <Jonvickay@gmail.com>
2022-08-12 05:36:01 +0100

    Signup, login styled, routes introduced, Admin created

--- app.js
+++ app.js
@@ -2,7 +2,19 @@ const path = require("path");
 
 const express = require("express");
 
+const csrf = require("csurf");
+const expressSession = require("express-session")
+
+const createSessionConfig = require("./config/session")
+
+const db = require("./data/database");
+const addCsrfTokenMiddleware = require("./middlewares/csrf-token")
+const errorHandlingMiddleware = require("./middlewares/error-handler")
+const checkAuthStatusMiddleware = require("./middlewares/check-auth");
+
 const authRoutes = require("./routes/auth.routes")
+const productsRoutes = require("./routes/products.route");
+const baseRoutes = require("./routes/base.route");
 
 const app = express();
 
@@ -10,7 +22,28 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false}));
 
+const sessionConfig = createSessionConfig();
+
+app.use(expressSession(sessionConfig));
+
+app.use(csrf());
+
+app.use(addCsrfTokenMiddleware);
+app.use(checkAuthStatusMiddleware);
+
+app.use(baseRoutes);
 app.use(authRoutes);
+app.use(productsRoutes);
 
-app.listen(3000);
+app.use(errorHandlingMiddleware)
+
+db.connectToDatabase().then(function() {
+    app.listen(3000);
+})
+.catch(function(error) {
+    console.log("Failed to connect to the Database");
+    console.log(error);
+});
+

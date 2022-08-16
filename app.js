09049da1d1cb19b6c15eec39ab7ac5479282b8e4
JustKel <Jonvickay@gmail.com>
2022-08-16 07:28:01 +0100

    Completed the cart sess

--- app.js
+++ app.js
@@ -12,11 +12,13 @@ const addCsrfTokenMiddleware = require("./middlewares/csrf-token")
 const errorHandlingMiddleware = require("./middlewares/error-handler")
 const checkAuthStatusMiddleware = require("./middlewares/check-auth");
 const protectRoutesMiddleware = require('./middlewares/protect-routes');
+const cartMiddleware = require("./middlewares/cart");
 
 const authRoutes = require("./routes/auth.routes")
 const productsRoutes = require("./routes/products.route");
 const baseRoutes = require("./routes/base.route");
 const adminRoutes = require("./routes/admin.routes");
+const cartRoutes = require("./routes/cart.routes");
 
 const app = express();
 
@@ -27,6 +29,7 @@ app.use(express.static("public"));
 app.use( '/products/assets', express.static("product-data"));
 
 app.use(express.urlencoded({ extended: false}));
+app.use(express.json());
 
 const sessionConfig = createSessionConfig();
 
@@ -34,12 +37,15 @@ app.use(expressSession(sessionConfig));
 
 app.use(csrf());
 
+app.use(cartMiddleware);
+
 app.use(addCsrfTokenMiddleware);
 app.use(checkAuthStatusMiddleware);
 
 app.use(baseRoutes);
 app.use(authRoutes);
 app.use(productsRoutes);
+app.use("/cart", cartRoutes);  // added here because it should also be viewed by unauth people
 app.use(protectRoutesMiddleware); 
 app.use("/admin", adminRoutes);
 
